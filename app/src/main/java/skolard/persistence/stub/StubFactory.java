ffa03c9effe71769df028cb55712bd89fd621524
MatthewYablonski <yablonsm@myumanitoba.ca>
2025-05-19 22:12:04 -0500

    Create stub factory

--- app/src/main/java/skolard/persistence/stub/StubFactory.java
+++ app/src/main/java/skolard/persistence/stub/StubFactory.java
@@ -5,5 +5,15 @@ import skolard.persistence.StudentPersistence;
 import skolard.persistence.TutorPersistence;
 
 public class StubFactory {
-    
+    public static SessionPersistence createSessionPersistence() {
+        return new SessionStub(); // in-memory note persistence
+    }
+
+    public static StudentPersistence createStudentPersistence() {
+        return new StudentStub(); // in-memory category persistence
+    }
+
+    public static TutorPersistence createTutorPersistence() {
+        return new TutorStub(); // in-memory category persistence
+    }
 }
