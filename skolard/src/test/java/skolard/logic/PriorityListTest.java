657131532e70e90e2c549a051b8714a86f22680c
Anton Wang <antonwcbt@gmail.com>
2025-05-20 02:39:08 -0500

    replaced the import of PriorityList with package for Junit 5 as declared in InteliJ Gradle template

--- skolard/src/test/java/skolard/logic/PriorityListTest.java
+++ skolard/src/test/java/skolard/logic/PriorityListTest.java
@@ -1,10 +1,11 @@
+package skolard.logic;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import skolard.logic.PriorityList;
+//import skolard.logic.PriorityList;
 
 public class PriorityListTest {
     private PriorityList<String> priorityList;
