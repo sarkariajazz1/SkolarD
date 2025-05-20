cafbbb2746ebe1a318e401e6b18f2a9eb862062c
Anton Wang <wangc16@myumanitoba.ca>
2025-05-19 20:58:14 -0500

    Updated unit tests in PriorityListTest class

--- app/src/test/java/skolard/logic/PriorityListTest.java
+++ app/src/test/java/skolard/logic/PriorityListTest.java
@@ -1,10 +1,10 @@
-package skolard.logic;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
+
+import skolard.logic.PriorityList;
 
 public class PriorityListTest {
     private PriorityList<String> priorityList;
