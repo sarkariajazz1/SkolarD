9d03b75ad271665874c1a7e83f61068365c8b5ba
John Aguinaldo <aguinal1@myumanitoba.ca>
2025-05-27 00:01:46 -0500

    Updated SessionHandler method name and created SessionHandlerTest

--- skolard/app/src/main/java/skolard/logic/SessionHandler.java
+++ skolard/app/src/main/java/skolard/logic/SessionHandler.java
@@ -18,12 +18,11 @@ public class SessionHandler {
     public void createSession(User user, LocalDateTime start, LocalDateTime end, String courseName){
         if(user instanceof Tutor){
             Tutor tutor = (Tutor) user;
-            //Discuss how session IDs should be made and how to access SessionStub generateID method
             sessionPersistence.addSession(new Session(-1, tutor, null, start, end, courseName));
         }
     }
 
-    public void bookSession(User user, int sessionID){
+    public void bookASession(User user, int sessionID){
         if(user instanceof Student){
             Student student = (Student) user;
             Session session = sessionPersistence.getSessionById(sessionID);
