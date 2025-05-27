72ae8d41db321c3045d747cfd0e73e881bf99651
MatthewYablonski <yablonsm@myumanitoba.ca>
2025-05-27 01:34:16 -0500

    Removed exception classes from sample project

--- skolard/app/src/main/java/skolard/exceptions/NoteSearchException.java
+++
@@ -1,8 +0,0 @@
-package skolard.exceptions;
-
-/** Thrown when a search operation does not find any matching notes. */
-public class NoteSearchException extends RuntimeException {
-    public NoteSearchException(String message) {
-        super(message);
-    }
-}
