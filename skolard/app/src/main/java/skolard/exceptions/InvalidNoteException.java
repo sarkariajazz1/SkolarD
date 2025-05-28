6dcb5354d2cabce7c22951d05af5dad40c9df6bf
Anton Wang <wangc16@myumanitoba.ca>
2025-05-28 16:10:18 -0500

    Clear up main branch

--- skolard/app/src/main/java/skolard/exceptions/InvalidNoteException.java
+++
@@ -1,8 +0,0 @@
-package skolard.exceptions;
-
-/** Thrown when attempting to add or modify a note with invalid data. */
-public class InvalidNoteException extends RuntimeException {
-    public InvalidNoteException(String message) {
-        super(message);
-    }
-}
