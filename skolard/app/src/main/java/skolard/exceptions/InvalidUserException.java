b52700301d0a44a86709c8d7315b1b01d5de3934
MatthewYablonski <yablonsm@myumanitoba.ca>
2025-05-27 22:31:42 -0500

    removed unused exception classes

--- skolard/app/src/main/java/skolard/exceptions/InvalidUserException.java
+++
@@ -1,8 +0,0 @@
-package skolard.exceptions;
-
-/** Thrown when attempting to add or modify a note with invalid data. */
-public class InvalidUserException extends RuntimeException {
-    public InvalidUserException(String message) {
-        super(message);
-    }
-}
--- skolard/app/src/main/java/skolard/exceptions/MessageExistsException.java
+++
@@ -1,8 +0,0 @@
-package skolard.exceptions;
-
-/** Thrown when attempting to add or modify a note with invalid data. */
-public class MessageExistsException extends RuntimeException {
-    public MessageExistsException(String message) {
-        super(message);
-    }
-}
