b96189e25f4945c0050d47b697ad954d4af7c1b1
John Aguinaldo <aguinal1@myumanitoba.ca>
2025-05-30 13:39:03 -0500

    Updated validateNumber in PaymentHandler

--- skolard/app/src/main/java/skolard/logic/PaymentHandler.java
+++ skolard/app/src/main/java/skolard/logic/PaymentHandler.java
@@ -13,6 +13,10 @@ public class PaymentHandler {
         boolean validExpiry = false;
         boolean validCVV = false;
 
+        validNumber = validateNumber(number.replaceAll("\\s+", ""));
+        validExpiry = validateExpiry(expiry.replaceAll("\\s+", ""));
+        validCVV = validateCVV(cvv.replaceAll("\\s+", ""));
+
         if(validNumber && validExpiry && validCVV){
             return true;
         } else{
@@ -20,16 +24,61 @@ public class PaymentHandler {
         }
     }
 
-    public boolean validateNumber(){
+    public boolean validateNumber(String number){
+        boolean validNumber = true;
+        long sameDigits = number.chars().distinct().count();
 
+        //Check if number is between lengths of 13 and 19
+        if(number.length() < 13 && number.length() > 19){
+            validNumber = false;
+        }
+
+        //Checks if number passes Luhn's algorithm 
+        if(!isValidLuhn(number)){
+            return false;
+        }
+
+        //Checks if all digits are the same.
+        if(sameDigits == 1){
+            validNumber = false;
+        }
+
+
+        return validNumber;
     }
 
-    public boolean validateExpiry(){
+    public static boolean isValidLuhn(String number) {
+        int sum = 0;
+        boolean doubleDigit = false;
 
+        // Process digits from right to left
+        for (int i = number.length() - 1; i >= 0; i--) {
+            char c = number.charAt(i);
+            if (!Character.isDigit(c)) return false; // Invalid character
+
+            int digit = c - '0';
+            if (doubleDigit) {
+                digit *= 2;
+                if (digit > 9) digit -= 9;
+            }
+
+            sum += digit;
+            doubleDigit = !doubleDigit;
+        }
+
+        return (sum % 10 == 0);
     }
 
-    public boolean validateCVV(){
+    public boolean validateExpiry(String expiry){
+        boolean validExpiry = true;
 
+        return validExpiry;
+    }
+
+    public boolean validateCVV(String cvv){
+        boolean validCVV = true;
+
+        return validCVV;
     }
 
 }
