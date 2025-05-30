c03793ff0f78f06915b216dd4ac7e81c5cde158d
John Aguinaldo <aguinal1@myumanitoba.ca>
2025-05-30 13:15:55 -0500

    Initialized methods in PaymentHandler

--- skolard/app/src/main/java/skolard/logic/PaymentHandler.java
+++ skolard/app/src/main/java/skolard/logic/PaymentHandler.java
@@ -1,5 +1,35 @@
 package skolard.logic;
 
 public class PaymentHandler {
-    
+    //Insert private Database variable
+
+    //Default Constructor
+    public PaymentHandler(){
+        //Insert Database initialization
+    }
+
+    public boolean validateCard(String number, String expiry, String cvv){
+        boolean validNumber = false;
+        boolean validExpiry = false;
+        boolean validCVV = false;
+
+        if(validNumber && validExpiry && validCVV){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public boolean validateNumber(){
+
+    }
+
+    public boolean validateExpiry(){
+
+    }
+
+    public boolean validateCVV(){
+
+    }
+
 }
