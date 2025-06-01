0541be3e8eec2979f14657cd0012194264bd12da
Joshua Chuwang-Kwa <chuwangj@myumanitoba.ca>
2025-05-31 21:58:10 -0500

    Updated SkolardApp.java to add buttons for LoginView, SignUpView, and SupportView

--- skolard/app/src/main/java/skolard/presentation/SkolardApp.java
+++ skolard/app/src/main/java/skolard/presentation/SkolardApp.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import skolard.logic.FAQHandler;
 import skolard.logic.MatchingHandler;
 import skolard.logic.ProfileHandler;
+import skolard.logic.SupportHandler;
 
 import java.awt.*;
 
@@ -13,25 +14,37 @@ import java.awt.*;
  */
 public class SkolardApp extends JFrame {
 
-    public SkolardApp(ProfileHandler profileHandler, MatchingHandler matchingHandler) {
+    public SkolardApp(ProfileHandler profileHandler, MatchingHandler matchingHandler, SupportHandler supportHandler) {
         this(profileHandler, matchingHandler, new FAQHandler());
     }
 
     public SkolardApp(ProfileHandler profileHandler, MatchingHandler matchingHandler, FAQHandler faqHandler) {
-        super("SkolarD - Dashboard"); 
+        super("SkolarD - Dashboard");
 
         setLayout(new BorderLayout());
 
-        // Create navigation buttons for profile, matching, and FAQ views
-        JPanel buttonPanel = new JPanel(new GridLayout(1, 3, 10, 10));
+        // Create navigation buttons for all views
+        JPanel buttonPanel = new JPanel(new GridLayout(2, 3, 10, 10));
+        JButton loginBtn = new JButton("Login");
+        JButton signUpBtn = new JButton("Sign Up");
         JButton profileBtn = new JButton("View Profiles");
         JButton matchBtn = new JButton("Find Tutors");
+        JButton supportBtn = new JButton("Support");
         JButton faqBtn = new JButton("FAQs");
 
+        buttonPanel.add(loginBtn);
+        buttonPanel.add(signUpBtn);
         buttonPanel.add(profileBtn);
         buttonPanel.add(matchBtn);
+        buttonPanel.add(supportBtn);
         buttonPanel.add(faqBtn);
         add(buttonPanel, BorderLayout.CENTER);
+
+        // Open Login View
+        loginBtn.addActionListener(e -> new LoginView(profileHandler));
+
+        // Open Sign Up View
+        signUpBtn.addActionListener(e -> new SignUpView(profileHandler));
 
         // Open Profile View
         profileBtn.addActionListener(e -> new ProfileView(profileHandler));
@@ -39,11 +52,14 @@ public class SkolardApp extends JFrame {
         // Open Matching View
         matchBtn.addActionListener(e -> new MatchingView(matchingHandler));
 
+        // Open Support View
+        //supportBtn.addActionListener(e -> new SupportView(supportHandler));
+
         // Open FAQ View
         faqBtn.addActionListener(e -> new FAQView(faqHandler));
 
         setDefaultCloseOperation(EXIT_ON_CLOSE); // Full app exits
-        setSize(500, 150);
+        setSize(600, 200);
         setLocationRelativeTo(null); // Center
         setVisible(true);
     }
