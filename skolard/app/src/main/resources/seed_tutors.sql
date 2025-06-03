228c1756a3716a73653bfbed125dba0ff40b0a0b
Jaskaran <sarkari2@myumanitoba.ca>
2025-06-03 15:31:34 -0500

    Updated seed

--- skolard/app/src/main/resources/seed_tutors.sql
+++ skolard/app/src/main/resources/seed_tutors.sql
@@ -4,6 +4,6 @@ INSERT OR IGNORE INTO tutor (email, name, bio, password) VALUES
 ('sukhdeep@example.com', 'Sukhdeep Kaur', 'Math tutor with 5 years of experience.', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); -- password: password
 
 INSERT OR IGNORE INTO tutor (email, name, bio, password) VALUES
-('amrit@example.com', 'Amrit Singh', 'Expert in physics and calculus.', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); -- password: 123456
+('amrit@example.com', 'Amrit Singh', 'Expert in physics and calculus.', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); -- password: password
 
 COMMIT;
