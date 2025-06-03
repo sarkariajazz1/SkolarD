c85e9434569621223af7202f1569c0688a882eb7
Jaskaran <sarkari2@myumanitoba.ca>
2025-06-03 15:25:51 -0500

    Fixed seeding big for tutors and students

--- skolard/app/src/main/resources/seed_tutors.sql
+++ skolard/app/src/main/resources/seed_tutors.sql
@@ -1,7 +1,9 @@
 BEGIN TRANSACTION;
 
 INSERT OR IGNORE INTO tutor (email, name, bio, password) VALUES
-('sukhdeep@example.com', 'Sukhdeep Kaur', 'Math tutor with 5 years of experience.', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'), -- password: password
-('amrit@example.com', 'Amrit Singh', 'Expert in physics and calculus.', '6cb75f652a9b52798eb6cf2201057c73e0679d7d2d3c6c7a3ad7a8a99d60f8b1'); -- password: 123456
+('sukhdeep@example.com', 'Sukhdeep Kaur', 'Math tutor with 5 years of experience.', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); -- password: password
+
+INSERT OR IGNORE INTO tutor (email, name, bio, password) VALUES
+('amrit@example.com', 'Amrit Singh', 'Expert in physics and calculus.', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); -- password: 123456
 
 COMMIT;
