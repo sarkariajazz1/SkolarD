2979ed04dc14f5a65ee69faea0b2d04e764c1195
John Aguinaldo <aguinal1@myumanitoba.ca>
2025-06-06 14:37:44 -0500

    Added a way to unbook sessions

--- skolard/app/src/main/java/skolard/logic/session/SessionHandler.java
+++ skolard/app/src/main/java/skolard/logic/session/SessionHandler.java
@@ -32,31 +32,27 @@ public class SessionHandler {
      * @param end the end time of the session
      * @param courseName the name of the course for the session
      */
-    public void createSession(User user, LocalDateTime start, LocalDateTime end, String courseName){
-        if(user instanceof Tutor){
-            Tutor tutor = (Tutor) user;
-            
-            // Get all sessions for the tutor
-            List<Session> tutorSessions = sessionPersistence.getSessionsByTutorEmail(tutor.getEmail());
+    public void createSession(Tutor tutor, LocalDateTime start, LocalDateTime end, String courseName){
+        // Get all sessions for the tutor
+        List<Session> tutorSessions = sessionPersistence.getSessionsByTutorEmail(tutor.getEmail());
 
-            // Check for scheduling conflicts with existing sessions
-            for(int i = 0; i < tutorSessions.size(); i++){
-                LocalDateTime tutorStartTime = tutorSessions.get(i).getStartDateTime();
-                LocalDateTime tutorEndTime = tutorSessions.get(i).getEndDateTime();
+        // Check for scheduling conflicts with existing sessions
+        for(int i = 0; i < tutorSessions.size(); i++){
+            LocalDateTime tutorStartTime = tutorSessions.get(i).getStartDateTime();
+            LocalDateTime tutorEndTime = tutorSessions.get(i).getEndDateTime();
 
-                // Conflict if new session overlaps with existing session
-                if(start.isAfter(tutorStartTime) && start.isBefore(tutorEndTime) 
-                        || end.isAfter(tutorStartTime) && end.isBefore(tutorEndTime)){
-                    throw new IllegalArgumentException("Session conflicts within existing sessions");
-                } else if(tutorStartTime.isAfter(start) && tutorStartTime.isBefore(end) 
-                        || tutorEndTime.isAfter(start) && tutorEndTime.isBefore(end)){
-                    throw new IllegalArgumentException("Existing sessions conflicts within session");
-                }
+            // Conflict if new session overlaps with existing session
+            if(start.isAfter(tutorStartTime) && start.isBefore(tutorEndTime) 
+                    || end.isAfter(tutorStartTime) && end.isBefore(tutorEndTime)){
+                throw new IllegalArgumentException("Session conflicts within existing sessions");
+            } else if(tutorStartTime.isAfter(start) && tutorStartTime.isBefore(end) 
+                    || tutorEndTime.isAfter(start) && tutorEndTime.isBefore(end)){
+                throw new IllegalArgumentException("Existing sessions conflicts within session");
             }
-
-            // No conflicts, add new session with unassigned student (null), Session id is -1 and will auto increment in database
-            sessionPersistence.addSession(new Session(-1, tutor, null, start, end, courseName));
         }
+
+        // No conflicts, add new session with unassigned student (null), Session id is -1 and will auto increment in database
+        sessionPersistence.addSession(new Session(-1, tutor, null, start, end, courseName));
     }
 
     /**
@@ -65,25 +61,32 @@ public class SessionHandler {
      * @param user the user attempting to book the session (must be a Student)
      * @param sessionID the ID of the session to be booked
      */
-    public void bookASession(User user, int sessionID){
-        if(user instanceof Student){
-            Student student = (Student) user;
+    public void bookASession(Student student, int sessionID){
+        // Retrieve the session to be booked
+        Session session = sessionPersistence.getSessionById(sessionID);
 
-            // Retrieve the session to be booked
-            Session session = sessionPersistence.getSessionById(sessionID);
-
-            // Check if the session is already booked
-            if(!session.isBooked()){
-                session.bookSession(student); // Book the session for the student
-                sessionPersistence.updateSession(session);
+        // Check if the session is already booked
+        if(!session.isBooked()){
+            session.bookSession(student); // Book the session for the student
+            sessionPersistence.updateSession(session);
+        } else {
+            // Session is booked — check if by the same student or another
+            if(session.getStudent().equals(student)){
+                throw new IllegalArgumentException("Session is already booked");
             } else {
-                // Session is booked — check if by the same student or another
-                if(session.getStudent().equals(user)){
-                    throw new IllegalArgumentException("Session is already booked");
-                } else {
-                    throw new IllegalArgumentException("Session is already booked by someone else");
-                }
+                throw new IllegalArgumentException("Session is already booked by someone else");
             }
+        }
+    }
+
+    public void unbookASession(Student student, int sessionID){
+        Session session = sessionPersistence.getSessionById(sessionID);
+
+        if(session.isBooked()){
+            session.unbookSession(student);
+            sessionPersistence.updateSession(session);
+        } else {
+            throw new IllegalArgumentException("Session has not been booked");
         }
     }
 }
