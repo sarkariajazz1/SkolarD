e91a61c480fc4dcdcd6e41511bee34b8860cc366
Anton Wang <wangc16@myumanitoba.ca>
2025-06-09 17:36:50 -0500

    Delete everything from dev

--- skolard/app/src/main/resources/seed_students.sql
+++
@@ -1,9 +0,0 @@
-BEGIN TRANSACTION;
-
-INSERT OR IGNORE INTO student (email, name) VALUES
-('alice@example.com', 'Alice'),
-('bob@example.com', 'Bob'),
-('simran@example.com', 'Simran Dhillon'),
-('raj@example.com', 'Raj Gill');
-
-COMMIT;
