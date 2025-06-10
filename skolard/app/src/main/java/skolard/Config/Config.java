af55dfbc6abf43e5d890de1a48b070e3a02cfe8e
Anton Wang <antonwcbt@gmail.com>
2025-06-10 16:54:37 -0500

    renamed the setup folder and created config file

---
+++ skolard/app/src/main/java/skolard/Config/Config.java
@@ -0,0 +1,16 @@
+package skolard.Config;
+
+public class Config {
+    public static final String BASE_URL = "https://api.skolard.com";
+    public static final String API_VERSION = "v1";
+    public static final String API_URL = BASE_URL + "/" + API_VERSION;
+
+    public static final String AUTH_TOKEN = "auth_token";
+    public static final String USER_ID = "user_id";
+
+    public static final int TIMEOUT_SECONDS = 30;
+    public static final int MAX_RETRIES = 3;
+
+    // Prevent instantiation
+    private Config() {}
+}
